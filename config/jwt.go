949f6723639f471024f82c804ed1ef9d0404f2cd
fdcwh <1291713293@qq.com>
2023-07-26 08:16:13 +0800

    初始

--- config/jwt.go
+++
@@ -1,8 +0,0 @@
-package config
-
-type JWT struct {
-	SigningKey  string `yaml:"signingKey"`  // jwt签名
-	ExpiresTime string `yaml:"expiresTime"` // 过期时间
-	BufferTime  string `yaml:"buffer-time"` // 缓冲时间
-	Issuer      string `yaml:"issuer"`      // 签发者
-}
--- config/storage_local.go
+++
@@ -1,6 +0,0 @@
-package config
-
-type Local struct {
-	Path      string `yaml:"path"`       // 本地文件访问路径
-	StorePath string `yaml:"store-path"` // 本地文件存储路径
-}
